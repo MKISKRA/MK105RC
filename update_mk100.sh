4916646cf30ee6ced221aa09d1574d431ceff285
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2024-03-26 08:41:00 +0300

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; 
-cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T;
-chmod +x /root/mk100/mk100t/MK100T;
+pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T;
 cd /;
 /root/mk100/mk100t/start.sh
