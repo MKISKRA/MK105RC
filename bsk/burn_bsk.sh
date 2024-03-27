9b448d2d7614a3c3f703888c895f6a8b2556b58c
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2024-03-27 17:07:47 +0300

    Add files via upload

--- bsk/burn_bsk.sh
+++ bsk/burn_bsk.sh
@@ -1,1 +1,2 @@
-
+#!/bin/bash
+echo 1 > /sys/class/leds/ena_bskpwr/brightness && export PYTHONPATH=/root/ir_examples_1.0.275633b && cd /root/ir_examples_1.0.275633b &&python3 irc.py -c /dev/ttymxc3 -b 115200 update_firmware /root/mk100/MK100/bsk/reader-1.38.5786.bin.P.signed
