7d6396bb076f7effc0b7a8046652b945baf76312
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2024-07-12 11:12:20 +0300

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T;
+cp /root/mk100/MK100/sys/powercontrol.sh /root/mk100/mk100t/powercontrol.sh; chmod +x /root/mk100/mk100t/powercontrol.sh;
+cp /root/mk100/MK100/sys/modemcontrol.sh /root/mk100/mk100t/modemcontrol.sh; chmod +x /root/mk100/mk100t/modemcontrol.sh;
 cd /;
 /home/linaro/start.sh
