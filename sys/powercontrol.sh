217ba992882674f2b76cb66407698c9f3742e00b
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2024-08-16 10:34:14 +0300

    Add files via upload

--- sys/powercontrol.sh
+++ sys/powercontrol.sh
@@ -2,6 +2,7 @@
 
 # Initialize the counter
 z=1
+TIMEMESSG=30 # Show Period (sec) of message if CRITPWR (10 20 30 40 50 60)
 # Iterate the loop for 2 times
 while [ $z -le 2 ]
 do
@@ -17,28 +18,55 @@ while :
 do
 	/root/mk100/mk100t/modemcontrol.sh
 	CURPWR=`cat /sys/class/power_supply/bq27520-0/voltage_now`
+	MAXPWR="0"
+	TIMECNTR=0
 	CRITPWR="3200000"
 	LOWPWR="3300000"
 
-	#echo "Current power: $CURPWR"
+	for (( i=1; i <= 12; i++ ))
+	do
+		CURPWR=`cat /sys/class/power_supply/bq27520-0/voltage_now`
+		#echo "Current power: $CURPWR"
+		if [[ $CURPWR =~ ^[0-9]+$ ]] ; then
+			if [ $CURPWR -gt $MAXPWR ] ; then
+				MAXPWR=$CURPWR
+				#echo "Max power: $MAXPWR"
+			fi
 
-	if [[ $CURPWR =~ ^[0-9]+$ ]] ; then
+			if [ $TIMECNTR -ge $TIMEMESSG ]; then
+				#echo ">="
+				TIMECNTR=0
+				if [ $LOWPWR -ge $MAXPWR ] ; then
+					DISPLAY=:0 feh -F /usr/share/images/desktop-base/criticalpower.png &
+					sleep 5
+					pkill feh
+				fi
+			else#echo "<"
+			fi
+		fi
+		sleep 10
+		TIMECNTR=$(($TIMECNTR + 10))
+	done
 
-		if [ $CRITPWR -ge $CURPWR ] ; then
-			echo "Critical Power."	
+	if [[ $MAXPWR =~ ^[0-9]+$ ]] ; then
+
+		echo "Max power ====: $MAXPWR"
+
+		if [ $CRITPWR -ge $MAXPWR ] ; then
+			echo "Critical Power."
 			DISPLAY=:0 feh -F /usr/share/images/desktop-base/poweroff.png &
 			sleep 10
 			echo 1 > /sys/class/leds/charger_board_power_off/brightness
 			poweroff
 		fi
 
-		if [ $LOWPWR -ge $CURPWR ] ; then
-			DISPLAY=:0 feh -F /usr/share/images/desktop-base/criticalpower.png &
-			sleep 5
-			pkill feh
-		fi
+		#if [ $LOWPWR -ge $MAXPWR ] ; then
+		#	DISPLAY=:0 feh -F /usr/share/images/desktop-base/criticalpower.png &
+		#	sleep 5
+		#	pkill feh
+		#fi
 	fi
 
- 	sleep 20
+ 	sleep 5
 done
 # end
