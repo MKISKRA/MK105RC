42f70c41e4eb00403156e887659b50df874d39e6
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2025-12-02 11:44:16 +0300

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -2,6 +2,10 @@
 pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T;
 #cp /root/mk100/MK100/sys/powercontrol.sh /root/mk100/mk100t/powercontrol.sh; chmod +x /root/mk100/mk100t/powercontrol.sh;
 #cp /root/mk100/MK100/sys/modemcontrol.sh /root/mk100/mk100t/modemcontrol.sh; chmod +x /root/mk100/mk100t/modemcontrol.sh;
+systemctl stop tpucontrol;
+sleep 1;
+cp /root/mk100/MK100/sys/tpu_mk100 /root/mk100/mk100t/tpu_mk100
+chmod +x /root/mk100/mk100t/tpu_mk100;
 cp /root/mk100/MK100/sys/gpsd /etc/default/gpsd;
 cd /;
 /home/linaro/start.sh
