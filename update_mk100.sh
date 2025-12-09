117ad853db2acea5fe04706f87d6c6e2a9cb5711
MKISKRA <115628416+MKISKRA@users.noreply.github.com>
2025-12-09 11:53:47 +0300

    Update update_mk100.sh

--- update_mk100.sh
+++ update_mk100.sh
@@ -2,7 +2,9 @@
 pkill MK100T; cd /root/mk100/MK100/ ; git fetch --all ; git reset --hard ; git pull; cp /root/mk100/MK100/MK100T /root/mk100/mk100t/MK100T; chmod +x /root/mk100/mk100t/MK100T;
 #cp /root/mk100/MK100/sys/powercontrol.sh /root/mk100/mk100t/powercontrol.sh; chmod +x /root/mk100/mk100t/powercontrol.sh;
 #cp /root/mk100/MK100/sys/modemcontrol.sh /root/mk100/mk100t/modemcontrol.sh; chmod +x /root/mk100/mk100t/modemcontrol.sh;
-cp /root/mk100/MK100/sys/boot_check.sh /root/mk100/mk100t/boot_check.sh
+cp /root/mk100/MK100/sys/boot_check.sh /root/mk100/mk100t/boot_check.sh;
+chmod +x  /root/mk100/mk100t/boot_check.sh;
+sleep 1;
 systemctl stop tpucontrol;
 systemctl stop modemcontrol;
 sleep 1;
